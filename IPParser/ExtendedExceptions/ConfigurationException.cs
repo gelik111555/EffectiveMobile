32885e0b2e64964a4a9332894a6c9852d0d4d0b6
da4a <gelik111555@gmail.com>
2024-04-06 12:27:31 +0300

    feat: Add ConfigurationException class
    Adds a new ConfigurationException class to handle configuration-related exceptions. This class inherits from the Exception class and includes a property FilePath to store the path of the configuration file where the exception occurred.

---
+++ IPParser/ExtendedExceptions/ConfigurationException.cs
@@ -0,0 +1,12 @@
+﻿namespace IPParser.ExtendedExceptions;
+
+public class ConfigurationException : Exception
+{
+    public string FilePath { get; }
+
+    public ConfigurationException(string message, string filePath)
+        : base($"{message} Path: {filePath}")
+    {
+        FilePath = filePath;
+    }
+}
