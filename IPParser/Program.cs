3c8318f6e985ee032e95bda633fb687992fe860d
da4a <gelik111555@gmail.com>
2024-04-06 12:29:01 +0300

    feat: starts file

---
+++ IPParser/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static LogQueryParameters LoadParametersFromJson(string filePath)
+    {
+        var jsonText = File.ReadAllText(filePath);
+        var parameters = JsonConvert.DeserializeObject<LogQueryParameters>(jsonText);
+        return parameters;
+    }
+}
