416b36fb02ade1fae147c68f8a7069f76fec8748
redmibook <gelik111555@gmail.com>
2024-04-07 18:21:55 +0300

    feat: Implement IP parsing functionality in main program
    This commit refactors the Program class to implement the IP parsing functionality. The Main method orchestrates the execution by invoking methods to read log entries, filter them based on parameters, and write the results to an output file. It provides error handling for validation exceptions, argument exceptions, and general exceptions to ensure robustness.
    
    The ChooseInputMethod method allows the user to choose between loading parameters from a JSON file or entering them manually. It utilizes the ILogQueryConfigurator interface to load parameters from JSON or directly from user input.
    
    The ReadUserDataFromConsole method prompts the user to enter input parameters manually and constructs a UserDataDto object.
    
    The WriteLogEntriesToFile method writes filtered log entries to an output file using the FileWriter service.

--- IPParser/Program.cs
+++ IPParser/Program.cs
@@ -1,16 +1,113 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using FluentValidation;
+using IPParser.Interface;
+using IPParser.Models;
+using IPParser.Services;
 
-internal class Program
+class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            IFileReader _reader = new FileReader();
+            var parameters = ChooseInputMethod(_reader);
+            LogService logService = new(_reader);
+            var logs = logService.ReadLogEntriesFromFile(parameters.FileLog);
+            var results = LogService.FilterLogEntriesByParameters(logs, parameters);
+            WriteLogEntriesToFile(parameters.FileOutput, results);
+            Console.WriteLine("The work has been completed successfully");
+        }
+        catch(ValidationException ex)
+        {
+            foreach (var item in ex.Errors)
+            { 
+                Console.WriteLine($"Error: {item.AttemptedValue} - {item.ErrorMessage}");
+            }
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            var type = ex.GetType().ToString(); 
+            Console.WriteLine($"Exception type : {type}");
+            Console.WriteLine($"Error: {ex.Message}");
+        }
     }
 
-    public static LogQueryParameters LoadParametersFromJson(string filePath)
+    private static LogQueryParameters ChooseInputMethod(IFileReader _reader)
     {
-        var jsonText = File.ReadAllText(filePath);
-        var parameters = JsonConvert.DeserializeObject<LogQueryParameters>(jsonText);
-        return parameters;
+        ILogQueryConfigurator configurator = new LogQueryConfigurator(_reader);
+        while (true)
+        {
+            Console.WriteLine("Choose input method:");
+            Console.WriteLine("1 - Load from JSON file");
+            Console.WriteLine("2 - Enter data manually");
+            var inputMethod = Console.ReadLine();
+            switch (inputMethod)
+            {
+                case "1":
+                    while (true)
+                    {
+                        Console.WriteLine("Enter the path to the configuration file:");
+                        var filePath = Console.ReadLine();
+                        if (string.IsNullOrEmpty(filePath) || !Path.Exists(filePath)) // Используем !Path.Exists
+                        {
+                            Console.WriteLine("Incorrect file path, please try again.");
+                            continue;
+                        }
+                        return configurator.LoadParametersFromJson(filePath);
+                    }
+
+                case "2":
+                    var userData = ReadUserDataFromConsole();
+                    return LogQueryConfigurator.LoadParametersFromUserData(userData);
+
+                default:
+                    Console.WriteLine("Invalid input method selected, please try again.");
+                    continue;
+            }
+        }
+    }
+
+    private static UserDataDto ReadUserDataFromConsole()
+    {
+        Console.WriteLine("Enter File Log:");
+        var fileLog = Console.ReadLine();
+
+        Console.WriteLine("Enter File Output:");
+        var fileOutput = Console.ReadLine();
+
+        Console.WriteLine("Enter Address Start (optional):");
+        var addressStart = Console.ReadLine();
+
+        Console.WriteLine("Enter Address Mask (optional):");
+        var addressMask = Console.ReadLine();
+
+        Console.WriteLine("Enter Time Start (dd.MM.yyyy):");
+        var timeStart = Console.ReadLine();
+
+        Console.WriteLine("Enter Time End (dd.MM.yyyy):");
+        var timeEnd = Console.ReadLine();
+
+        return new UserDataDto
+        {
+            FileLog = fileLog,
+            FileOutput = fileOutput,
+            AddressStart = addressStart,
+            AddressMask = addressMask,
+            TimeStart = timeStart,
+            TimeEnd = timeEnd
+        };
+    }
+
+    private static void WriteLogEntriesToFile(string filePath, List<LogEntry> logEntries)
+    {
+        IFileWriter fileWriter = new FileWriter();
+        var lines = logEntries.Select
+            (entry => $"{entry.IpAddress}:{entry.AccessTime.ToString("yyyy-MM-dd HH:mm:ss")}").ToArray();
+        fileWriter.WriteAllLines(filePath, lines);
     }
 }
