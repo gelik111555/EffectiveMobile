3cdd1f5e8b50db9db2ed809cd18134154a07cfdc
redmibook <gelik111555@gmail.com>
2024-04-07 18:19:44 +0300

    feat: Add UserDataDto model config

--- IPParser/Models/UserDataDto.cs
+++ IPParser/Models/UserDataDto.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace IPParser.Models;
 
-public class UserDataDto
+public record UserDataDto
 {
+    [JsonProperty("file-log")]
+    public string FileLog { get; set; }
+
+    [JsonProperty("file-output")]
+    public string FileOutput { get; set; }
+
+    [JsonProperty("address-start")]
+    public string AddressStart { get; set; }
+
+    [JsonProperty("address-mask")]
+    public string AddressMask { get; set; }
+
+    [JsonProperty("time-start")]
+    public string TimeStart { get; set; }
+
+    [JsonProperty("time-end")]
+    public string TimeEnd { get; set; }
 }
