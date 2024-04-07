14e299b6da35766c50ca52ce59a3a81f175a9c36
redmibook <gelik111555@gmail.com>
2024-04-07 18:19:10 +0300

    feat: Define LogQueryParameters struct for IP parsing
    This commit introduces the LogQueryParameters struct in the IPParser.Models namespace. LogQueryParameters represents the parameters used for querying log data. It includes properties for file paths, IP addresses, address masks, and time ranges. Additionally, it provides a constructor that takes a UserDataDto object and validates its data using the UserDataDtoValidator before initializing the LogQueryParameters instance. This ensures that the provided user data is valid and can be used safely in the IP parsing process.

---
+++ IPParser/Models/LogQueryParameters.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using IPParser.Validators;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Net;
+
+namespace IPParser.Models;
+
+public readonly struct LogQueryParameters
+{
+    public string FileLog { get; }
+
+    public string FileOutput { get; }
+
+    public IPAddress? AddressStart { get; }
+
+    public int? AddressMask { get; }
+
+    public DateOnly TimeStart { get; }
+
+    public DateOnly TimeEnd { get; }
+
+    public LogQueryParameters(UserDataDto userDataDto)
+    {
+        var validator = new UserDataDtoValidator();
+        var validationResult = validator.Validate(userDataDto);
+
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException($"Validation failed", validationResult.Errors);
+        }
+        FileLog = userDataDto.FileLog;
+        FileOutput = userDataDto.FileOutput;
+
+        // Преобразование строки IP-адреса в объект IPAddress. Если строка пуста, AddressStart будет null.
+        AddressStart = !string.IsNullOrEmpty(userDataDto.AddressStart)
+            ? IPAddress.Parse(userDataDto.AddressStart)
+            : null;
+
+        // Преобразование строки маски в число. Если строка пуста, AddressMask будет null.
+        AddressMask = int.TryParse(userDataDto.AddressMask, out var mask)
+            ? mask
+            : null;
+
+        var culture = CultureInfo.InvariantCulture;
+        TimeStart = DateOnly.ParseExact(userDataDto.TimeStart, "dd.MM.yyyy", culture);
+        TimeEnd = DateOnly.ParseExact(userDataDto.TimeEnd, "dd.MM.yyyy", culture);
+    }
+
+}
