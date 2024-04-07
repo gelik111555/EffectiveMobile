e3feff1fb1b9a79fb2b60773b71455121a99dc9a
redmibook <gelik111555@gmail.com>
2024-04-07 18:17:37 +0300

    feat: Add interface for file writing operations

---
+++ IPParser/Interface/IFileWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPParser.Interface;
+
+internal interface IFileWriter
+{
+    void WriteAllText(string path, string text);
+    void WriteAllLines(string path, string[] lines);
+}
